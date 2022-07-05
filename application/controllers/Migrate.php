39afd2020ff1d7fac35ada740587879c12fff95f
C. Eduardo Carrasco <educamo@gmail.com>
2022-07-04 20:56:17 -0400

    modificacion controlador migrate

--- application/controllers/Migrate.php
+++ application/controllers/Migrate.php
@@ -2,6 +2,7 @@
     defined("BASEPATH") or exit("No direct script access allowed");
 
     class Migrate extends CI_Controller {
+        
         public function __construct()
             {
             parent::__construct();
